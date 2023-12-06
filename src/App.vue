74524bd85db8e384004107c24d48bf2a3103819a
pietrolupi <pietro.lupi92@gmail.com>
2023-12-06 16:20:33 +0100

    [add]: componente loader

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   import axios from 'axios';
   import {store} from  './data/store';
   import ProjectCard from './components/ProjectCard.vue';
+  import Loader from './components/Loader.vue';
 
 
   export default {
     name: 'App',
+    
 
     components: {
       ProjectCard,
+      Loader,
     },
 
     data() {
       return {
         store,
+        isLoading: true,
       }
     },
     methods: {
@@ -21,7 +25,8 @@
         axios.get(store.apiUrl + 'projects')
         .then(res=>{
           console.log(res.data);
-          store.projects = res.data;
+          store.projects = res.data.data;
+          this.isLoading = false;
         })
       }
     },
@@ -39,11 +44,24 @@
   </header>
 
   <main>
-    <ProjectCard />
+    <div v-if="isLoading" class="loader-container">
+      <Loader  />
+
+    </div>
+    <ProjectCard v-else />
    
   </main>
 </template>
 
 <style lang="scss">
-
+  header{
+    h1{
+      margin-top: 50px;
+      color: white;
+      font-weight: 900;
+    }
+  }
+  .loader-container {
+    margin-top: 120px;
+  }
 </style>
