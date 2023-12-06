81c7443c117e95e864431da5627fdd5f0f67d30d
pietrolupi <pietro.lupi92@gmail.com>
2023-12-06 14:39:02 +0100

    [init]: struttura iniziale vue

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+  import axios from 'axios';
+  import {store} from  './data/store';
+  import ProjectsMain from './components/ProjectsMain.vue';
+
+  
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+        store: store
+      }
+    },
+    methods: {
+      getApi(){
+        axios.get(store.apiUrl + 'projects').then(res=>{
+          console.log(res.data);
+        })
+      }
+    },
+    mounted() {
+      this.getApi();
+    }
+  }
+</script>
+
+<template>
+  <header>
+    <h1>Vite Boolfolio</h1>
+  </header>
+
+  <main>
+    <ProjectsMain />
+  </main>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/ProjectsMain.vue
@@ -0,0 +1,25 @@
+<script>
+  export default {
+
+    name:'ProjectsMain',
+    
+    data(){
+      return{
+        title:'Pagina Component Projects',
+      }
+    },
+  
+  }
+</script>
+
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <h2>ciaoooooooooo</h2>
+  </div>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
